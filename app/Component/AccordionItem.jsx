bc56cadf70b923039b4ada8681f3a34e00e444a8
sachin <sachin@sachins-MacBook-Air.local>
2025-11-14 14:27:15 +0530

    corrected imports

--- app/Component/AccordionItem.jsx
+++ app/Component/AccordionItem.jsx
@@ -1,3 +1,7 @@
+import { motion } from "framer-motion";
+import { FaPlus } from "react-icons/fa";
+
+
 const AccordionItem = ({ question, answer }) => {
   const [open, setOpen] = React.useState(false);
 
