8e531c42c3583e814c4571a22894ad23b76172cf
sachin <sachin@sachins-MacBook-Air.local>
2025-11-14 11:17:44 +0530

    apos corrected 1

--- app/contact/page.js
+++ app/contact/page.js
@@ -92,7 +92,7 @@ export default function Page() {
             We handle a wide range of legal matters
           </h2>
           <p className="text-gray-600 mb-6">
-            We prioritize our clients' needs, understanding that each case is unique.
+            We prioritize our clients&apos; needs, understanding that each case is unique.
             Our team of seasoned attorneys is committed to delivering tailored solutions,
             ensuring you receive the attention and representation you deserve.
             Our comprehensive expertise allows us to handle a wide range of legal matters effectively.
@@ -126,7 +126,7 @@ export default function Page() {
             Customer support and assistance
           </h2>
           <p className="text-gray-600">
-            We prioritize our clients' needs, understanding that each case is unique.
+            We prioritize our clients&apos; needs, understanding that each case is unique.
             Our team of seasoned attorneys is committed to delivering tailored solutions,
             ensuring you receive the attention and representation you deserve.
             Our comprehensive expertise allows us to handle a wide range of legal matters effectively.
